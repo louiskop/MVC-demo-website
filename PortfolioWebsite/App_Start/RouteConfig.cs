4edf33104d2722f37945eebc2233718bab47935c
Louis de Jager <louisdj12@gmail.com>
2019-02-27 19:59:03 +0200

    Finished navbar items Routing

--- PortfolioWebsite/App_Start/RouteConfig.cs
+++ PortfolioWebsite/App_Start/RouteConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -7,17 +8,34 @@ using System.Web.Routing;
 
 namespace PortfolioWebsite
 {
-    public class RouteConfig
-    {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+	public class RouteConfig
+	{
+		public static void RegisterRoutes(RouteCollection routes)
+		{
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
-        }
-    }
+			routes.MapRoute(
+				name: "Default",
+				url: "{controller}/{action}/{id}",
+				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+			);
+
+			routes.MapRoute(
+				name: "Projects and skills",
+				url: "{controller}/{action}/",
+				defaults: new { controller = "catagories", action = "ProjectsAndSkills" }
+				);
+
+			routes.MapRoute(
+				"Education and Qualifications",
+				"{controller}/{action}/",
+				new {controller = "catagories", action = "EducationAndQualifications" }
+
+
+
+
+
+			);
+		}
+	}
 }
---
+++ PortfolioWebsite/Controllers/catagoriesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortfolioWebsite.Controllers
+{
+	public class catagoriesController : Controller
+	{
+		// GET: catagories
+		public ActionResult ProjectsAndSkills()
+		{
+			return View();
+		}
+
+		public ActionResult EducationAndQualifications()
+		{
+			return View();
+		}
+	}
+}
